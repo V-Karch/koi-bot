7743355feabcb79e3114cde06013bc9000b9d8e5
V-Karch <kluna0905@gmail.com>
2024-09-23 11:35:57 -0400

    entertainment: added logs

--- cogs/entertainment.py
+++ cogs/entertainment.py
@@ -2,7 +2,7 @@ import random
 import discord
 import asyncio
 import requests
-from discord import app_commands
+from discord import HTTPException, InteractionResponded, NotFound, app_commands
 from discord.ext import commands
 from pythondebuglogger.Logger import Logger
 
@@ -22,7 +22,16 @@ class Entertainment(commands.Cog):
 
         hug_api_url = "https://api.otakugifs.xyz/gif?reaction=hug"
         data = requests.get(hug_api_url)
-        return data.json().get("url")
+
+        try:
+            return data.json().get("url")
+        except Exception as e:
+            logger.display_error(
+                f"[get_hug_gif()] failed to retrieve a valid json response: {e}"
+            )
+            logger.display_error(f"[get_hug_gif()] Actual response data:")
+            logger.display_debug(data.text)
+            return None
 
     @app_commands.command(name="hug", description="Give a user of your choice a hug")
     @app_commands.describe(user="The user you want to give a hug to")
@@ -32,10 +41,32 @@ class Entertainment(commands.Cog):
         It will also ping the member and attempt to access the interaction user's avatar.
         """
 
-        await interaction.response.defer()
+        try:
+            await interaction.response.defer()
+        except HTTPException:
+            logger.display_error(f"[Interaction {interaction.id}] failed to defer.")
+            return
+        except InteractionResponded:
+            logger.display_error(
+                f"[Interaction {interaction.id}] has already been responded to."
+            )
+            return
 
-        active_event_loop = asyncio.get_running_loop()
+        try:
+            active_event_loop = asyncio.get_running_loop()
+        except RuntimeError:
+            logger.display_error(
+                f"[{interaction.user.id}/hug] There is no running event loop."
+            )
+            return
+
         api_results = await active_event_loop.run_in_executor(None, self.get_hug_gif)
+
+        if api_results == None:
+            logger.display_error(
+                f"[{interaction.user.id}/hug] Failed to retrieve valid json from API"
+            )
+            return
 
         hug_message = f"*{interaction.user.name} is giving {user.name} a hug!*"
         if user == interaction.user or user == self.client.user:
@@ -48,14 +79,31 @@ class Entertainment(commands.Cog):
         try:
             avatar_url = interaction.user.avatar.url
         except Exception as e:
-            print(e)
+            logger.display_debug(
+                f"[{interaction.user.id}/hug] Something is up with the users avatar"
+            )
             avatar_url = ""
 
         hug_embed.set_footer(
             text=f"Hugs from {interaction.user.name}!", icon_url=avatar_url
         )
 
-        await interaction.followup.send(user.mention, embed=hug_embed)
+        try:
+            await interaction.followup.send(user.mention, embed=hug_embed)
+        except HTTPException:
+            logger.display_error(f"[{interaction.user.id}/hug] Message failed to send.")
+        except NotFound:
+            logger.display_error(
+                f"[{interaction.user.id}/hug] This webhook was not found."
+            )
+        except TypeError:
+            logger.display_error(
+                f"[{interaction.user.id}/hug] You specified both embed and embeds or file and files or thread and thread_name."
+            )
+        except ValueError:
+            logger.display_error(
+                f"{interaction.user.id}/hug The length of embeds was invalid, there was no token associated with this webhook or ephemeral was passed with the improper webhook type or there was no state attached with this webhook when giving it a view."
+            )
 
     @app_commands.command(name="flip", description="flip a coin")
     async def _flip(self, interaction: discord.Interaction):
@@ -68,14 +116,23 @@ class Entertainment(commands.Cog):
         logger.display_notice(
             f"[{interaction.user.id}] generated random number ({random_number})"
         )
-        await interaction.response.send_message(
-            f"{interaction.user.mention} {'heads' if random_number else 'tails'}"
-        )
-        logger.display_notice(f"[{interaction.user.id}] reply message sent")
+        try:
+            await interaction.response.send_message(
+                f"{interaction.user.mention} {'heads' if random_number else 'tails'}"
+            )
+            logger.display_notice(
+                f"Successfully sent reply message to {interaction.channel.id}"
+            )
+        except Exception as e:
+            logger.display_error(f"{interaction.user.id} {e}")
 
     @app_commands.command(name="ping", description="Pong! 🏓")
     async def _ping(self, interaction: discord.Interaction):
-        await interaction.response.send_message("Pong! 🏓", ephemeral=True)
+        logger.display_notice(f"[{interaction.user.id}] is calling /ping")
+        try:
+            await interaction.response.send_message("Pong! 🏓", ephemeral=True)
+        except Exception as e:
+            logger.display_error(f"[{interaction.user.id}] {e}")
 
 
 async def setup(client: commands.Bot) -> None:
