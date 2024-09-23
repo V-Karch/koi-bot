216348d1a7d1c74cddabc3aa6cc3bc821b0901d9
V-Karch <luna.karch05@gmail.com>
2024-09-23 13:30:12 -0400

    logger_help: added send_response_message_with_logs() function

--- logger_help.py
+++ logger_help.py
@@ -20,6 +20,7 @@ async def defer_with_logs(
     Returns:
         bool: True if the defer was successful, False otherwise
     """
+
     try:
         await interaction.response.defer(ephemeral=ephemeral)
         logger.display_notice(f"[{interaction.user.id}] is having a command defered.")
@@ -30,5 +31,42 @@ async def defer_with_logs(
     except discord.InteractionResponded:
         logger.display_error(
             f"[Interaction {interaction.id}] has already been responded to."
+        )
+        return False
+
+
+async def send_response_message_with_logs(
+    interaction: discord.Interaction,
+    logger: Logger,
+    command_name: str,
+    message: str,
+    ephemeral: bool = False,
+):
+    try:
+        message: discord.Message = await interaction.response.send_message(
+            message, ephemeral=ephemeral
+        )
+        logger.display_notice(
+            f"[User {interaction.user.id}/{command_name}] Successfully sent reply message to [Channel {interaction.channel.id}]"
+        )
+        return message
+    except discord.HTTPException:
+        logger.display_error(
+            f"[User {interaction.user.id}/{command_name}] Message failed to send."
+        )
+        return False
+    except discord.NotFound:
+        logger.display_error(
+            f"[User {interaction.user.id}/{command_name}] This webhook was not found."
+        )
+        return False
+    except TypeError:
+        logger.display_error(
+            f"[User {interaction.user.id}/{command_name}] You specified both embed and embeds or file and files or thread and thread_name."
+        )
+        return False
+    except ValueError:
+        logger.display_error(
+            f"User {interaction.user.id}/{command_name} The length of embeds was invalid, there was no token associated with this webhook or ephemeral was passed with the improper webhook type or there was no state attached with this webhook when giving it a view."
         )
         return False
